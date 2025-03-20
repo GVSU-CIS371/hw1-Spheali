b0b37ee96bf6f8b53591d9e55055a150d351b7fa
Spheali <cowan.gavin04@gmail.com>
2025-03-19 22:11:40 -0400

    Fixed not filled bug
    Fixed the cup not being filled fully if no syrup was selected

--- src/components/Syrup.vue
+++ src/components/Syrup.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="syrup" :style="{backgroundColor:currentSyrup.color}"></div>
+  <div class="syrup" :style="{backgroundColor:currentBase.color}" v-if="currentSyrup.color === '#c6c6c6'"></div>
+  <div class="syrup" :style="{backgroundColor:currentSyrup.color}" v-else></div>
 </template>
 
 <script setup lang="ts">
